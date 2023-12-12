c7a1d5a4856be377f8fc1d0c386b4a16bc264619
Ruth1318 <124644895+Ruth1318@users.noreply.github.com>
2023-12-11 21:55:21 -0600

    Add files via upload

---
+++ parcial Api Rick end Morty/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 900,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadFile('app/index.html');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate',()=>{
+    if (mainWindow==null) {
+        createWindow();
+    }
+})
